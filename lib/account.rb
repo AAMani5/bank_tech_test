690ecce9f68fca77c7a375fa9474df994d20c960
Ashwini <ashie.oh@gmail.com>
2017-04-18 15:58:12 +0100

    Test drive ceation of deposit and withdraw methods

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Account
   attr_reader :balance, :transactions
 
@@ -5,6 +7,7 @@ class Account
     @name = accout_details[:name]
     @balance = 0
     @transactions = []
+    @transaction = accout_details[:transaction] || Struct.new(:amount, :date)
   end
 
   def name
@@ -14,6 +17,18 @@ class Account
   def deposit(amount)
     fail "Please make sure that deposit amount is positive integer" if amount < 0
     @balance += amount
+    new_transaction = transaction.new(amount, DateTime.now)
+    transactions.push(new_transaction)
   end
+
+  def withdraw(amount)
+    fail "Please make sure balance amount does not go below zero." if (balance - amount) < 0
+    @balance -= amount
+    new_transaction = transaction.new(-amount, DateTime.now)
+    transactions.push(new_transaction)
+  end
+
+  private
+  attr_reader :transaction
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,8 @@ require 'account'
 
 describe Account do
   let(:account) {Account.new(:name => :ash)}
+  let(:deposit_amount) {10}
+  let(:withdraw_amount) {5}
 
   it "has a name" do
     expect(account.name).to eql 'ash'
@@ -18,7 +20,25 @@ describe Account do
   context "#deposit" do
 
     it "increases the balance amount" do
-      expect{account.deposit(10)}.to change{account.balance}.by(10)
+      expect{account.deposit(deposit_amount)}.to change(account, :balance).by(deposit_amount)
+    end
+
+    it "adds a transaction to existing list" do
+      expect{account.deposit(deposit_amount)}.to change(account.transactions, :count).by(1)
+    end
+
+  end
+
+  context "withdraw" do
+
+    before (:each) {account.deposit(deposit_amount)}
+
+    it "decreases the balance amount" do
+      expect{account.withdraw(withdraw_amount)}.to change(account, :balance).by(-withdraw_amount)
+    end
+
+    it "adds a transaction to existing list" do
+      expect{account.withdraw(withdraw_amount)}.to change(account.transactions, :count).by(1)
     end
 
   end
