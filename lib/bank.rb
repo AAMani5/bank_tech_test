f621b53d737561797be43bf7bd549d9be3655aad
Ashwini <ashie.oh@gmail.com>
2017-04-18 14:33:25 +0100

    Test drive creation of the create_account method

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,5 +1,9 @@
 class Bank
 
+  def initialize(bank_details)
+    @name = bank_details[:name]
+  end
+
   def create_account(account_details)
   end
 
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,6 +2,20 @@ require 'bank'
 
 describe Bank do
 
-  it {is_expected.to respond_to(:create_account).with(1).argument}
+  let(:bank) {Bank.new(:name => :tsb)}
+  let(:account) {double :account}
+
+  context "#create_account" do
+    it "takes account details as parameter" do
+      expect{bank.create_account(:name => :ash)}.not_to raise_error
+    end
+
+    it "returns an account object" do
+      allow(bank).to receive(:create_account).and_return(account)
+      my_account = bank.create_account(:name => :ash)
+      expect(my_account).to eql(account)
+    end
+
+  end
 
 end
