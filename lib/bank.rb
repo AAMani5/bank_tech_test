98951c220ef339f5dd1a30f0ce74663ea30b88d7
Ashwini <ashie.oh@gmail.com>
2017-04-18 17:59:45 +0100

    Extracted a private method

--- lib/bank.rb
+++ lib/bank.rb
@@ -19,11 +19,15 @@ class Bank
   end
 
   def print_statement(account)
-    fail "Sorry, we could not find your account in our bank" if !accounts.include?(account)
+    fail "Sorry, we could not find your account in our bank" unless has_account?(account)
     printer.print_statement(account)
   end
 
   private
   attr_reader :account_class, :printer
+
+  def has_account?(account)
+    accounts.include?(account)
+  end
 
 end
