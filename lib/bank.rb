55a0ad58f331a95496aafd3c260f442d2de53d66
Ashwini <ashie.oh@gmail.com>
2017-04-18 14:54:47 +0100

    Add logic to create account and add it to list

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,14 +1,23 @@
 class Bank
+  attr_reader :accounts
 
   def initialize(bank_details)
     @name = bank_details[:name]
+    @account_class = bank_details[:account_class]
+    @accounts = []
   end
 
   def create_account(account_details)
+    new_account = account_class.new(account_details[:name])
+    accounts.push(new_account)
+    new_account
   end
 
   def name
     @name.to_s
   end
+
+  private
+  attr_reader :account_class
 
 end
