1ffe565b2258cedc931aee6c6a362e81c55ebb3c
Ashwini <ashie.oh@gmail.com>
2017-04-18 18:49:18 +0100

    Add logic to accept an optional date with amount

--- lib/account.rb
+++ lib/account.rb
@@ -14,20 +14,24 @@ class Account
     @name.to_s
   end
 
-  def deposit(amount)
+  def deposit(amount_details)
+    amount = amount_details[:amount]
     fail "Please make sure the deposit amount is an integer" if not_integer? amount
     fail "Please make sure that deposit amount is positive integer" if negative_amount? amount
     @balance += amount
-    new_transaction = transaction_class.new(amount, DateTime.now, @balance)
+    date = get_date(amount_details[:date])
+    new_transaction = transaction_class.new(amount, date, @balance)
     transactions.push(new_transaction)
   end
 
-  def withdraw(amount)
+  def withdraw(amount_details)
+    amount = amount_details[:amount]
     fail "Please make sure the amount to withdraw is an integer" if not_integer? amount
     fail "Please make sure that amount to withdraw is positive integer" if negative_amount? amount
     fail "Please make sure balance amount does not go below zero." if balance_negative? amount
     @balance -= amount
-    new_transaction = transaction_class.new(-amount, DateTime.now, @balance)
+    date = get_date(amount_details[:date])
+    new_transaction = transaction_class.new(-amount, date, @balance)
     transactions.push(new_transaction)
   end
 
@@ -44,6 +48,10 @@ class Account
 
   def negative_amount?(amount)
     amount < 0
+  end
+
+  def get_date(date)
+    date ? DateTime.parse(date) : DateTime.now
   end
 
 end
